f5ea48d6db5fbbe6dad745a1448195416d508c33
Vedant <vedantkingh@gmail.com>
2023-12-19 14:27:16 +0530

    UI

--- src/app/components/ContactCard.js
+++ src/app/components/ContactCard.js
@@ -1,12 +1,14 @@
 import styles from '@/app/contact/contact.module.css'
 import Link from 'next/link';
+import { MdEmail } from 'react-icons/md'
+
 const ContactCard = () => {
     return (
         <div className={styles.section}>
             <div className={styles.container}>
                 <div className={styles.grid}>
                     <div className={styles.grid_card}>
-                        <i></i>
+                        <i><MdEmail/></i>
                         <h2>Email</h2>
                         <p>Monday to Friday Expected</p>
                         <p className={styles.last_para}>response time: 72 hours</p>

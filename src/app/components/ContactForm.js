85a79922cb7a8d0966f732c23c3727855072de67
Vedant <vedantkingh@gmail.com>
2023-12-20 00:39:11 +0530

    UI

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -1,5 +1,7 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import styles from '@/app/contact/contact.module.css'
+import common from '@/app/styles/common.module.css'
 import { Mulish } from 'next/font/google'
  
 const mulish = Mulish({
@@ -9,25 +11,49 @@ const mulish = Mulish({
 
 
 const ContactForm = () => {
+    const[user, setUser] = useState({
+        username : "",
+        email : "",
+        phone : "",
+        message : ""
+    }) 
+
+    function handleChange(e){
+        const name = e.target.name;
+        const value = e.target.value;
+        setUser((prevUser) => ({...prevUser, [name] : value}))
+    }
+
+    function handleSubmit(e){
+
+    }
+
     return (
-        <form className={styles.contact_form}>
+        <form className={styles.contact_form} onSubmit={handleSubmit}>
             <div className={styles.input_field}>
                 <label htmlFor="username" className={styles.label}>
                     Name:
-                    <input type='text' name='username' id='username' placeholder='Enter your phoneame' className={mulish.className}/>
+                    <input type='text' name='username' id='username' placeholder='Enter your phone' className={mulish.className}
+                    value={user.username} onChange={handleChange} required/>
                 </label>
                 <label htmlFor="email" className={styles.label}>
                     E-mail:
-                    <input type='text' name='email' id='email' placeholder='Enter your E-mail' className={mulish.className}/>
+                    <input type='text' name='email' id='email' placeholder='Enter your E-mail' className={mulish.className}
+                    value={user.email} onChange={handleChange} required/>
                 </label>
                 <label htmlFor="phone" className={styles.label}>
                     Phone:
-                    <input type='number' name='phone' id='phone' placeholder='Enter your Phone' className={mulish.className}/>
+                    <input type='number' name='phone' id='phone' placeholder='Enter your Phone' className={mulish.className}
+                    value={user.phone} onChange={handleChange} required/>
                 </label>
                 <label htmlFor="message" className={styles.label}>
                     Message:
-                    <textarea rows={5} type='text' name='message' id='message' placeholder='Enter your Message' className={mulish.className}/>
+                    <textarea rows={5} type='text' name='message' id='message' placeholder='Enter your Message' 
+                    className={mulish.className} value={user.message} onChange={handleChange} required/>
                 </label>
+            </div>
+            <div>
+                <button type='submit' className={common.fancyButton}>Send Message</button>    
             </div>      
         </form>
     );
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -12,6 +12,8 @@ const page = () => {
                 <h2>We&apos;d love to hear <span>from you</span></h2>
             </section>
             <ContactForm/>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3784.187320448626!2d73.88916667496173!3d18.475172382609593!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2ea62d51980df%3A0x2852517b24c2f948!2sHill%20Mist%20Garden%2C%20NIBM%20Road%2C%20Kausar%20Baugh%2C%20Kondhwa%2C%20Pune%2C%20Maharashtra%20411048%2C%20India!5e0!3m2!1sen!2suk!4v1703012753706!5m2!1sen!2suk" 
+            width={600} height={450} style={{border:0}} loading="lazy" referrerpolicy="no-referrer-when-downgrade" className={styles.mapping}></iframe>
         </div>
     );
 };

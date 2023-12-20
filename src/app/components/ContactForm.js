c1fd385523986c53a2b626e1379faf966e9554e7
Vedant <vedantkingh@gmail.com>
2023-12-20 20:29:05 +0530

    API

--- src/app/components/ContactForm.js
+++ src/app/components/ContactForm.js
@@ -24,50 +24,104 @@ const ContactForm = () => {
         setUser((prevUser) => ({...prevUser, [name] : value}))
     }
 
-    const handleSubmit =  async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
         try {
-            const response = await fetch ('/api/contact', {
-                method: 'POST',
-                headers: {"Content_Type": "application/json"},
+            const response = await fetch('/api/contact', {
+                method:'POST',
+                headers:{"Content_Type":"application/json"},
                 body: JSON.stringify({
-                    username: user.username,
-                    email: user.email,
-                    phone: user.phone,
-                    message: user.message,
+                    username:user.username,
+                    email:user.email,
+                    phone:user.phone,
+                    message:user.message
                 })
             })
-        } catch (error) {
-            console.log(error)
+            // Set the status based on the response from the API route
+            if (response.status === 200) {
+                setUser({
+                    username: "",
+                    email: "",
+                    phone: "",
+                    message: ""
+                })
+                setStatus('success');
+            } else {
+                setStatus('error');
+            }
+
+        }catch (e) {
+            console.log(e)
         }
+
     }
+
+
 
     return (
         <form className={styles.contact_form} onSubmit={handleSubmit}>
             <div className={styles.input_field}>
                 <label htmlFor="username" className={styles.label}>
-                    Name:
-                    <input type='text' name='username' id='username' placeholder='Enter your phone' className={mulish.className}
-                    value={user.username} onChange={handleChange} required/>
-                </label>
-                <label htmlFor="email" className={styles.label}>
-                    E-mail:
-                    <input type='text' name='email' id='email' placeholder='Enter your E-mail' className={mulish.className}
-                    value={user.email} onChange={handleChange} required/>
-                </label>
-                <label htmlFor="phone" className={styles.label}>
-                    Phone:
-                    <input type='number' name='phone' id='phone' placeholder='Enter your Phone' className={mulish.className}
-                    value={user.phone} onChange={handleChange} required/>
-                </label>
-                <label htmlFor="message" className={styles.label}>
-                    Message:
-                    <textarea rows={5} type='text' name='message' id='message' placeholder='Enter your Message' 
-                    className={mulish.className} value={user.message} onChange={handleChange} required/>
+                    Enter your name
+                    <input type="text" name="username" id="username"
+                        placeholder="Enter your name"
+                           className={mulish.className}
+                           value={user.username}
+                           onChange={handleChange}
+                           required
+                    />
                 </label>
             </div>
+
+            <div className={styles.input_field}>
+                <label htmlFor="email" className={styles.label}>
+                    Email
+                    <input type="text" name="email" id="email"
+                           placeholder="Enter your email"
+                           className={mulish.className}
+                           value={user.email}
+                           onChange={handleChange}
+                           required
+                           autoComplete="off"
+                    />
+                </label>
+            </div>
+
+            <div className={styles.input_field}>
+                <label htmlFor="phone" className={styles.label}>
+                    Phone Number
+                    <input type="number" name="phone" id="phone"
+                           placeholder="Enter your phone"
+                           className={mulish.className}
+                           value={user.phone}
+                           onChange={handleChange}
+                           required
+                            autoComplete="off"
+                    />
+                </label>
+            </div>
+
+            <div className={styles.input_field}>
+                <label htmlFor="message" className={styles.label}>
+                    Message
+                    <textarea  name="message" id="message" rows={5}
+                           placeholder="Enter your Message"
+                           className={mulish.className}
+                               value={user.message}
+                               onChange={handleChange}
+                               required
+                                autoComplete="off"
+                    />
+                </label>
+            </div>
+
             <div>
-                <button type='submit' className={common.fancyButton}>Send Message</button>    
-            </div>      
+                {status === 'success' && <p className={styles.success_msg}>Thank you for your message!</p>}
+                {status === 'error' && <p className={styles.error_msg}>There was an error submitting your message. Please try again.</p>}
+
+                <button type="submit" className={mulish.className}>Send Message</button>
+            </div>
         </form>
     );
 };

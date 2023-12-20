cb353148a209bfe34e9c73a216da055f4a28e1fb
Vedant <vedantkingh@gmail.com>
2023-12-20 20:12:03 +0530

    API

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,2 +1,24 @@
 import dbConnect from "@/utils/dbConn";
 import Contact from "@/models/contact";
+import { NextResponse } from "next/server";
+
+export async function POST(req, res){
+    try {
+        const body =  res.json();
+        await dbConnect;
+
+        await Contact.create(body);
+
+        return NextResponse.json({
+            message: "Message sent successfully!"
+        },{
+            status: 200
+        })
+    } catch (error) {
+        return NextResponse.json({
+            message: "Server error, please try again!"
+        },{
+            status: 500
+        })
+    }
+}
